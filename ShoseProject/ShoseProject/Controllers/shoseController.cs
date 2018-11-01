09c1b8c3b7d10e6b3b715970c538b21a55dd06a0
Peeraki Aumsaard <n.incubus@hotmail.com>
2018-11-01 22:08:22 +0700

    commit shose

---
+++ ShoseProject/ShoseProject/Controllers/shoseController.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShoseProject.Models;
+using System.Data.Entity;
+using System.IO;
+
+
+namespace ShoseProject.Controllers
+{
+    public class shoseController : Controller
+    {
+        // GET: shose
+
+        shoseShopDBEntities db = new shoseShopDBEntities();
+        public ActionResult Index()
+        {
+            var ProdList = db.tbl_detail.Include(p => p.tbl_typestyle);
+            return View(ProdList.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.typeStyleID = new SelectList(db.tbl_typestyle, "typeStyleID", "typeName");
+            ViewBag.typeColourID = new SelectList(db.tbl_typeColour, "typeColourID", "typeColour");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(tbl_detail NewProduct, HttpPostedFileBase file)
+        {
+            string myfile = Path.GetFileName(file.FileName);
+            file.SaveAs(Server.MapPath("~/Imageshoes/" + myfile));
+            if (ModelState.IsValid)
+            {
+                NewProduct.image = "~/Imageshoes/" + myfile;
+                db.tbl_detail.Add(NewProduct);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.typeStyleID = new SelectList(db.tbl_typestyle, "typeStyleID", "typeName");
+            ViewBag.typeColour = new SelectList(db.tbl_typeColour, "typeColourID", "typeColour");
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ ShoseProject/ShoseProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShoseProject.MvcApplication" Language="C#" %>
